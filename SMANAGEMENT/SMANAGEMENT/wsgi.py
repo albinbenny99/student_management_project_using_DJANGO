9ce6355c6ec4013d9aa03278ce6bd4715f30dc6b
ALBIN BENNY <albin99.in@gmail.com>
2024-01-22 20:13:18 +0530

    project completed

---
+++ SMANAGEMENT/SMANAGEMENT/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for SMANAGEMENT project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SMANAGEMENT.settings')
+
+application = get_wsgi_application()
